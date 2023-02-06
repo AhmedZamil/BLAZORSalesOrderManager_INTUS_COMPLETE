eb00476a2dbaadfdab7b29016a4615ecc22fb73e
AhmedZamil <ahmedlutfulzamil@gmail.com>
2023-02-07 02:19:41 +0600

    SET up file rewrite

--- SalesOrderManager.Api/Setup.bat
+++ SalesOrderManager.Api/Setup.bat
@@ -9,6 +9,12 @@ echo  packages restored successfully
 dotnet build
 echo  packages build successfully 
 
+cd..
+SalesOrderManager.DAL
+
+dotnet restore
+dotnet build
+
 dotnet ef --help
 
 dotnet tool install --global dotnet-ef --version 6.0.7
@@ -17,6 +23,8 @@ dotnet ef --help
 
 dotnet ef database update
 
+cd..
+cd SalesOrderManager.Api
 dotnet run
 
-echo  browse Application in browser : http://localhost:5000/
+echo  browse Application in browser : https://localhost:7039/  
